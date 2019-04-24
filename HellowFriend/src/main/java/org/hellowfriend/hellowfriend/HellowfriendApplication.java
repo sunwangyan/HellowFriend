ac679bc1443eea653632d6bf9df9e971ba0539cb
sy <xdb168@qq.com>
2019-04-24 16:47:45 +0800

    项目创建

---
+++ HellowFriend/src/main/java/org/hellowfriend/hellowfriend/HellowfriendApplication.java
@@ -0,0 +1,13 @@
+package org.hellowfriend.hellowfriend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellowfriendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HellowfriendApplication.class, args);
+    }
+
+}
---
+++ HellowFriend/src/test/java/org/hellowfriend/hellowfriend/HellowfriendApplicationTests.java
@@ -0,0 +1,16 @@
+package org.hellowfriend.hellowfriend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HellowfriendApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
